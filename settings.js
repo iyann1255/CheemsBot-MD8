1ded5b3688742213d968dba25f61639d990e504a
iyann1255 <tessbarca1981@gmail.com>
2023-10-21 19:40:57 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,27 +21,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.owner = ['6289516947204'] //ur owner number
+global.ownernomer = "6289516947204" //ur owner number2
+global.ownername = "🦄Dream Guy taruna" //ur owner name
+global.ytname = "YT: taruna" //ur yt chanel name
+global.socialm = "GitHub: taruna" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
+global.botname = "Cheems Bot taruna V8"
+global.ownernumber = '6289516947204'
+global.ownername = '🦄드림 가이 taruna'
+global.ownerNumber = ["6289516947204@s.whatsapp.net"]
+global.ownerweb = "https://youtube.com/@taruna"
+global.websitex = "https://youtu.be/taruna"
+global.wagc = "https://chat.whatsapp.com/"
 global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
+global.wm = "taruna Bot Inc."
+global.botscript = 'https:' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "🦄드림 가이 taruna\n\n"
+global.creator = "6289516947204@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
